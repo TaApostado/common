13dfabc8f3531d19de44834128c69b0d4a1cc698
Guilherme <guipython2001@gmail.com>
2022-09-13 22:07:04 -0300

    add: notification pattern for validations

---
+++ notification/notification.go
@@ -0,0 +1,69 @@
+package notification
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Notification struct {
+	context, message string
+}
+
+func NewNotification(context, message string) *Notification {
+	return &Notification{context: context, message: message}
+}
+
+type INotificator interface {
+	HasNotifications() bool
+	AddNotification(Notification)
+	Notifications() string
+}
+
+type Notificator struct {
+	notifications []Notification
+}
+
+func NewNotificator() *Notificator {
+	return &Notificator{notifications: []Notification{}}
+}
+
+func (n Notificator) HasNotifications() bool {
+	return len(n.notifications) > 0
+}
+
+func (n *Notificator) AddNotification(notification *Notification) {
+	if n.notifications == nil {
+		n.notifications = []Notification{}
+	}
+	n.notifications = append(n.notifications, *notification)
+}
+
+func (n Notificator) Notifications() string {
+	var sb strings.Builder
+	var notification Notification
+	index := 0
+
+	if !n.HasNotifications() {
+		return ""
+	}
+
+	if len(n.notifications) == 1 {
+		notification = n.notifications[index]
+		sb.WriteString(
+			fmt.Sprintf("%s: %s", notification.context, notification.message))
+		return sb.String()
+	}
+
+	for index < len(n.notifications)-1 {
+		notification = n.notifications[index]
+		sb.WriteString(
+			fmt.Sprintf("%s: %s, ", notification.context, notification.message))
+		index++
+	}
+
+	fmt.Println(index)
+	notification = n.notifications[index]
+	sb.WriteString(
+		fmt.Sprintf("%s: %s", notification.context, notification.message))
+	return sb.String()
+}
---
+++ notification/notification_test.go
@@ -0,0 +1,64 @@
+package notification
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddNewNotification(t *testing.T) {
+	notificator := NewNotificator()
+	assert.Equal(
+		t, len(notificator.notifications),
+		0, "new notificator should has 0 notification`s")
+
+	notificator.AddNotification(NewNotification("context", "some error..."))
+	assert.Equal(
+		t, len(notificator.notifications),
+		1, "after add one notification notificator should has 1 notification`s")
+
+	notificator.AddNotification(NewNotification("context", "some error..."))
+	assert.Equal(
+		t, len(notificator.notifications),
+		2, "after add one notification notificator should has 2 notification`s")
+}
+
+func TestHasNotifications(t *testing.T) {
+	notificator := NewNotificator()
+
+	assert.Equal(
+		t, false,
+		notificator.HasNotifications(), "should return false because notificator hasn`t notification`s")
+
+	notificator.notifications = append(
+		notificator.notifications, *NewNotification("context", "some error..."))
+	assert.Equal(
+		t, true,
+		notificator.HasNotifications(), "should return true because notificator has 1 notification`s")
+
+	notificator.notifications = append(
+		notificator.notifications, *NewNotification("other context", "some error..."))
+	assert.Equal(
+		t, true,
+		notificator.HasNotifications(), "should return true because notificator has 2 notification`s")
+}
+
+func TestNotifications(t *testing.T) {
+	notificator := NewNotificator()
+
+	assert.Equal(
+		t, "",
+		notificator.Notifications(), "notificator message malformed")
+
+	notificator.notifications = append(
+		notificator.notifications, *NewNotification("context", "some error..."))
+	assert.Equal(
+		t, "context: some error...",
+		notificator.Notifications(), "notificator message malformed")
+
+	notificator.notifications = append(
+		notificator.notifications, *NewNotification("other context", "some error..."))
+	assert.Equal(
+		t, "context: some error..., other context: some error...",
+		notificator.Notifications(), "notificator message malformed")
+}
